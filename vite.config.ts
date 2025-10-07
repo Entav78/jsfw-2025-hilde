e22715ef96dbafd056406a115135a99e0c1fe43e
Entav78 <hkljos_238@hotmail.com>
2025-10-07 12:36:28 +0200

    removed a .

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
